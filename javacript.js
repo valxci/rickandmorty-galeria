376e5786c493c2df9f04fa4a2817476fa5fdca64
valxci <valentinacastanoibarra@gmail.com>
2025-04-21 09:24:58 -0500

    avance1

---
+++ javacript.js
@@ -0,0 +1,35 @@
+const galeria = document.querySelector("#galeria");
+
+function createPost(name, message, imageURL, index) {
+  const div = document.createElement("div");
+  div.classList.add("digimon");
+
+  const nombre = document.createElement("h3");
+  nombre.textContent = name;
+  
+  const elemento = document.createElement("img");
+  elemento.src = imageURL;
+  
+  const mensaje = document.createElement("p");
+  mensaje.innerText = Este personaje es mi personaje  ${index};
+ 
+
+  
+  div.append(elemento, nombre, mensaje, );
+  return div;
+}
+
+
+for (let index = 1; index <= 826; index++) {
+  fetch (https://rickandmortyapi.com/api/character/${index})
+    .then(response => response.json())
+    .then(personaje => {
+      const post = createPost(
+        Personaje ${index}: ${personaje.name},
+        Especie: ${personaje.species} | Origen: ${personaje.origin.name},
+        personaje.image, index
+      );
+      galeria.appendChild(post);
+    })
+  
+}
